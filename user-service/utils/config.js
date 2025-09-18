9f9c32c064c74ce74ae5af1f04a86d730f532dc7
saurabhxcod <offical.saurabhsingh@gmail.com>
2025-09-18 11:29:39 +0530

    Fix JWT_SECRET fallback value in config

--- user-service/utils/config.js
+++ user-service/utils/config.js
@@ -3,5 +3,5 @@ module.exports = {
     MONGO_PASSWORD: process.env.MONGO_PASSWORD || 'admin',
     MONGO_DB_NAME: process.env.MONGO_DB_NAME || 'userdb',
     PORT: process.env.PORT || 1234,
-    JWT_SECRET: process.env.JWT_SECRET
+    JWT_SECRET: process.env.JWT_SECRET|| '#^#%^#^^#%&'
 }
